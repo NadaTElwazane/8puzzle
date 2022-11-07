7ac488c04bfeefee71c63572608876fe0a4540c0
NadaTElwazane <nadaelwazane@gmail.com>
2022-11-07 13:41:29 +0200

    added comments

--- BFS.py
+++ BFS.py
@@ -13,21 +13,28 @@ class BFS:
         frontier_q = []
         explored = set()
         parent_map = {}
+        # use a set for frontier for O(1) lookup and a queue for FIFO
         frontier_s.add(start)
         frontier_q.append(start)
         while frontier_s:
+            # remove the first state in the queue
             s = frontier_q.pop(0)
             frontier_s.remove(s)
             v = s.value
+            # add the state to explored
             explored.add(s)
-            depth = max(depth, s.depth)            
+            # compare the depth of the state with the current max depth
+            depth = max(depth, s.depth) 
+            # check if the state is the goal           
             if s == goal:
                 print("Goal achieved")
                 print(v)
                 break
+            # get the children of the state
             array = s.children()            
             for child in array:
                 if child not in explored and child not in frontier_s:
+                    # add the child to the frontier and the parent map
                     frontier_s.add(child)
                     frontier_q.append(child)
                     parent_map[child] = s
@@ -42,16 +49,20 @@ class BFS:
         BFS.path(parent_map, start, s)
 
     def path(parent_map, start, goal):
+        # get the path from the parent map 
         path_a = []
         parent = goal
         cost = 0
+        # while loop terminates when the parent is the start state
         while parent != start:
             BFS.array_gui.append(parent.value)
             path_a.append(parent.value)
             parent = parent_map.get(parent)
         print("Cost", goal.cost)
         print("Cost:", len(path_a))
+        # reverse the path to get the correct order
         path_a.reverse()
+        # BFS array is used to display the path in the GUI
         BFS.array_gui.reverse()
         print(path_a)
 
--- Board_gui.py
+++ Board_gui.py
@@ -16,7 +16,7 @@ from PyQt5 import QtCore as qtc
 class BoardWindow(qtw.QDialog):
     array=[]
     #start_val = 125340678
-    start_val = 724506831
+    start_val = 724508631
     goal_val = 12345678
     count = 0
 
